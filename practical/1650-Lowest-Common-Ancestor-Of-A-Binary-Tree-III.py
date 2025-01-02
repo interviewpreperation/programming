4273e9c3f7ce94b8bc9764ca889d395c2e05fd4c
Peter Lorenz <jS5t3r@users.noreply.github.com>
2025-01-02 10:00:26 +0000

    add practical

---
+++ practical/1650-Lowest-Common-Ancestor-Of-A-Binary-Tree-III.py
@@ -0,0 +1,23 @@
+# https://github.com/doocs/leetcode/blob/8380f3faa3965229fed73d62de85994aa690d41d/solution/1600-1699/1650.Lowest%20Common%20Ancestor%20of%20a%20Binary%20Tree%20III/README_EN.md
+
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+
+
+
+def lowestCommonAncestor(self, p: Node, q: Node) -> Node:
+    # https://www.youtube.com/watch?v=vZxxksAP8yk
+    
+    pass
+
+
+
+ res = lowestCommonAncestor(p: Node, q: Node)
+print(res.val)
