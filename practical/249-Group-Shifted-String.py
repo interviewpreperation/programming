0ec9d23460e3883dc26477c24ea9a8cc805e8f2e
Peter Lorenz <jS5t3r@users.noreply.github.com>
2025-03-27 23:39:11 +0800

    Update 249-Group-Shifted-String.py

--- practical/249-Group-Shifted-String.py
+++ practical/249-Group-Shifted-String.py
@@ -1,21 +1,20 @@
 # https://www.youtube.com/watch?v=g_CWHtPSQmQ
 
-
-
 from collections import defaultdict
+
 class Solution:
-  def groupStrings(strings):
-      def get_key(s):
-          key = []
-          for a, b in zip(s, s[1:]):
-              key.append((ord(b) - ord(a)) % 26)
-          return tuple(key)
-      
-      groups = {}
-      for s in strings:
-          key = get_key(s)
-          if key not in groups:
-              groups[key] = []
-          groups[key].append(s)
-      return list(groups.values())
+    def groupStrings(strings):
+        def get_key(s):
+            key = []
+            for a, b in zip(s, s[1:]):
+                key.append((ord(b) - ord(a)) % 26)
+            return tuple(key) # hashable for dict keys
+        
+        groups = {}
+        for s in strings:
+            key = get_key(s)
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(s)
+        return list(groups.values())
     
